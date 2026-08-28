cd094c6ba6d4103ef1c7a9d046e1eb7a225c6eb3
Toby <126118957+TxbiG@users.noreply.github.com>
2026-08-28 14:23:43 +0100

    Add thread management function declarations

--- library/thread.h
+++ library/thread.h
@@ -1,1 +1,7 @@
 
+void thread_spawn(void* thread, void* arg);
+void thread_join(void* thread);
+void thread_detach(void* thread);
+void thread_current_id();
+void thread_yield();
+void thread_sleep_ms(u64 milliseconds);
